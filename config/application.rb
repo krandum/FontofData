207f137690e34aebf140522a60b5f2e5673acf81
Pablo Alatorre <pablo.alatorre.t@gmail.com>
2017-09-17 06:16:25 -0700

    GET OUTA HERE

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,5 @@ module Fod
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-	config.generators.javascript_engine :js
   end
 end
