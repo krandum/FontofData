303feadbb347b506f7cc65f33c0aacd2eacb002d
Pablo Alatorre <pablo.alatorre.t@gmail.com>
2017-09-17 21:42:42 -0700

    hooking up controller

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,4 +1,12 @@
 class SiteController < ApplicationController
-  def index
-  end
+	def index
+		@nodes = []
+		(1...32).each do |iter|
+			name = "elem"
+			if (DataNode.where(value: iter).exists?)
+				name += " " + iter.to_s
+			end
+			@nodes.push(name)
+		end
+	end
 end
