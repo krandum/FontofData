18df218b6eea712244e6067cb92f81e7ce71c707
Pablo Alatorre <pablo.alatorre.t@gmail.com>
2017-09-17 23:05:03 -0700

    playing with controller

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,14 +1,14 @@
 class SiteController < ApplicationController
 	def index
-		@factions = ['', 'red', 'blue', 'green', ' ']
+		factions = ['', 'red', 'blue', 'green', ' ']
 		@nodes = []
 		iter = 1
 		while iter <= 32 do
 			name = 'elem'
-			node = DataNode.where(:value: iter).first
-			if node.method_defined?(:faction_id) AND !(node.empty?) AND defined? node
-				index = node.faction_id
-				name += ' ' << @factions[index]
+			elem = DataNode.where(:value: iter).first
+			if elem.method_defined?(:faction_id) AND !(elem.empty?) AND defined? elem
+				index = elem.faction.id
+				name += ' ' << factions[index]
 			end
 			@nodes.push(name)
 			iter += 1
