811b950e75e2be2065556f372b755eeeba3d8bc6
Pablo Alatorre <pablo.alatorre.t@gmail.com>
2017-09-17 23:39:14 -0700

    playing with controller

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,15 +1,15 @@
 class SiteController < ApplicationController
 	def index
-		factions = ['', 'red', 'blue', 'green', ' ']
+		factions = ["", "red", "blue", "green", ""]
 		@classes = []
 		iter = 1
 		while iter <= 32 do
 			print iter.to_s
-			name = 'elem'
+			name = "elem"
 			elem = DataNode.where(value: iter).first
 			unless elem.nil? || elem == 0
 				index = elem.faction_id
-				name += ' ' << factions[index]
+				name << " " << factions[index]
 			end
 			@classes.push(name)
 			iter += 1
