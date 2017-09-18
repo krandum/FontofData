c6d54583a6f94f7a9a5080655ca88c138ef05731
Pablo Alatorre <pablo.alatorre.t@gmail.com>
2017-09-17 22:29:04 -0700

    playing with controller

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,11 +1,11 @@
 class SiteController < ApplicationController
 	def index
-		@factions = [' ', ' red', ' blue', ' green']
+		@factions = [' ', ' red', ' blue', ' green', ' ']
 		@nodes = []
 		iter = 1
 		while iter <= 32 do
 			base = 'elem'
-			fid = DataNode.find_by(value: iter).first.faction_id
+			fid = DataNode.where(value: iter).first.faction_id
 			name = base << @factions[fid]
 			@nodes.push(name)
 			iter++
