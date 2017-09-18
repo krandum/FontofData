37e2f65f88a3e90b4757343038a2254522ca0095
Pablo Alatorre <pablo.alatorre.t@gmail.com>
2017-09-17 21:47:50 -0700

    playing with controller

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,10 +1,12 @@
 class SiteController < ApplicationController
 	def index
+		@factions = ["", "red", "blue", "green"]
 		@nodes = []
 		(1...32).each do |iter|
 			name = "elem"
-			if (DataNode.where(value: iter).exists?)
-				name += " " + iter.to_s
+			node = DataNode.where(value: iter).first
+			if (node.exists?)
+				name += " " + @factions[node.first.faction_id % 4]
 			end
 			@nodes.push(name)
 		end
