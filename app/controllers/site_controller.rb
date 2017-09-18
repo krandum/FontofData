d1010ee946f3286fd355ca1db32dce0d2071438d
Pablo Alatorre <pablo.alatorre.t@gmail.com>
2017-09-17 23:18:27 -0700

    playing with controller

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -5,7 +5,7 @@ class SiteController < ApplicationController
 		iter = 1
 		while iter <= 32 do
 			name = 'elem'
-			elem = DataNode.where(:value: iter).first
+			elem = DataNode.where(value: iter).first
 			if elem.method_defined?(:faction_id) AND !(elem.empty?) AND defined? elem
 				index = elem.faction.id
 				name += ' ' << factions[index]
