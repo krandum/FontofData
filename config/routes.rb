b52581570d0d82e8ce33d7c88f0fc69b0679ef9b
Pablo Alatorre <pablo.alatorre.t@gmail.com>
2017-09-19 14:59:59 -0700

    setting up devise

--- config/routes.rb
+++ config/routes.rb
@@ -30,7 +30,7 @@ Rails.application.routes.draw do
   put 'data_nodes/update'
   delete 'data_nodes/destroy'
 
-  root 'site#index'
+  root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
