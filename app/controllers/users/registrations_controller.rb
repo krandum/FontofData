45ca2f73f988e3df7bf7b8eb0e6677440bde4ba2
Pablo Alatorre <pablo.alatorre.t@gmail.com>
2017-09-19 17:51:55 -0700

    setting up devise

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -26,7 +26,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def faction
     @user = User.find(current_user.id)
     @user.update(params.permit(:faction_id))
-    redirect_to root_path
+    redirect_to 'site/index'
   end
 
   # DELETE /resource
