65c04e619bfe58f5a6eff85289c26114abc2fa42
Pablo Alatorre <pablo.alatorre.t@gmail.com>
2017-09-19 22:07:20 -0700

    hooking up interactions

--- app/controllers/interactions_controller.rb
+++ app/controllers/interactions_controller.rb
@@ -1,5 +1,5 @@
 class InteractionsController < ApplicationController
-	before_action :set_interaction, only: [:show, :edit, :update, :destroy, :take_action]
+	before_action :set_interaction, only: [:show, :edit, :update, :destroy]
 
 	# GET /interactions
 	# GET /interactions.json
