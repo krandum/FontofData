6216069dd59ce96f466df06e4a37539be22d6625
Pablo Alatorre <pablo.alatorre.t@gmail.com>
2017-09-19 19:48:44 -0700

    setting up controllers

--- config/routes.rb
+++ config/routes.rb
@@ -33,7 +33,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  get 'take_action' => 'interactions_controller#take_action'
+  take_action 'take_action', :controller => 'interaction', :action => 'take_action'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
