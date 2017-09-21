85499cecf804017856eee066f855d38c21b31a93
Pablo Alatorre <pablo.alatorre.t@gmail.com>
2017-09-20 22:30:54 -0700

    marging

--- config/routes.rb
+++ config/routes.rb
@@ -1,92 +1,89 @@
 Rails.application.routes.draw do
-  get 'home/index'
+	get 'home/index'
+	get 'play/index'
 
-  get 'play/index'
+	resources :interactions
+	resources :effects
+	resources :factions
+	resources :data_nodes
+	resources :play
 
-  resources :interactions
-  resources :effects
-  resources :factions
-  resources :data_nodes
-  resources :play
+	Rails.application.routes.draw do
+		get 'home/index'
+		get 'play/index'
+		devise_for :users,
+		controllers: { sessions: 'users/sessions' , registrations: 'users/registrations'}
+	end
 
-  Rails.application.routes.draw do
-  get 'home/index'
+	devise_scope :user do
+		patch 'registration/faction', :to => 'users/registrations#faction'
+	end
 
-  get 'play/index'
+	get 'data_nodes/index'
+	get 'data_nodes/show'
+	get 'data_nodes/new'
+	get 'data_nodes/edit'
+	post 'data_nodes/create'
+	patch 'data_nodes/update'
+	put 'data_nodes/update'
+	delete 'data_nodes/destroy'
 
-    devise_for :users,
-     controllers: { sessions: 'users/sessions' , registrations: 'users/registrations'}
-  end
+	root 'home#index'
 
-  devise_scope :user do
-    patch 'registration/faction', :to => 'users/registrations#faction'
-  end
+	get '/take_action', :to => 'interactions#take_action'
 
-  get 'data_nodes/index'
-  get 'data_nodes/show'
-  get 'data_nodes/new'
-  get 'data_nodes/edit'
-  post 'data_nodes/create'
-  patch 'data_nodes/update'
-  put 'data_nodes/update'
-  delete 'data_nodes/destroy'
+	# The priority is based upon order of creation: first created -> highest priority.
+	# See how all your routes lay out with "rake routes".
 
-  root 'home#index'
+	# You can have the root of your site routed with "root"
+	# root 'welcome#index'
 
-  get '/take_action', :to => 'interactions#take_action'
+	# Example of regular route:
+	#	 get 'products/:id' => 'catalog#view'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+	# Example of named route that can be invoked with purchase_url(id: product.id)
+	#	 get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+	# Example resource route (maps HTTP verbs to controller actions automatically):
+	#	 resources :products
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+	# Example resource route with options:
+	#	 resources :products do
+	#	 member do
+	#	 get 'short'
+	#	 post 'toggle'
+	#	 end
+	#
+	#	 collection do
+	#	 get 'sold'
+	#	 end
+	#	 end
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+	# Example resource route with sub-resources:
+	#	 resources :products do
+	#	 resources :comments, :sales
+	#	 resource :seller
+	#	 end
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+	# Example resource route with more complex sub-resources:
+	#	 resources :products do
+	#	 resources :comments
+	#	 resources :sales do
+	#	 get 'recent', on: :collection
+	#	 end
+	#	 end
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+	# Example resource route with concerns:
+	#	 concern :toggleable do
+	#	 post 'toggle'
+	#	 end
+	#	 resources :posts, concerns: :toggleable
+	#	 resources :photos, concerns: :toggleable
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+	# Example resource route within a namespace:
+	#	 namespace :admin do
+	#	 # Directs /admin/products/* to Admin::ProductsController
+	#	 # (app/controllers/admin/products_controller.rb)
+	#	 resources :products
+	#	 end
 end
