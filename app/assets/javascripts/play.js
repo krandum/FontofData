5d04451d30559a487f0ecc62b15b6042f717a4cb
Pablo Alatorre <pablo.alatorre.t@gmail.com>
2017-09-21 18:29:28 -0700

    working on paper

--- app/assets/javascripts/play.js
+++ app/assets/javascripts/play.js
@@ -125,8 +125,10 @@ $(document).on('ready page:load', function() {
 			datatype: "html",
 			success: function (data) {
 				console.log(data);
-				in_nodes = data['nodes'];
+				in_nodes = data["nodes"];
 				console.log(in_nodes);
+				console.log(data.nodes);
+				console.log(data['nodes']);
 				in_nodes.forEach(function(node) {
 					nodes[node['value']] = node['faction_id'];
 				console.log(nodes);
