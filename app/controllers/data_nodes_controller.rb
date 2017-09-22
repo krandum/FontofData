499344afec46b44a8ba562ce43908a49e66a4194
Pablo Alatorre <pablo.alatorre.t@gmail.com>
2017-09-21 18:20:48 -0700

    working on paper

--- app/controllers/data_nodes_controller.rb
+++ app/controllers/data_nodes_controller.rb
@@ -70,6 +70,8 @@ class DataNodesController < ApplicationController
 		while (i <= num)
 			range = ranges[i.to_s]
 			p range
+			p range[:from]
+			p range[0]
 			cur = range['from'].to_i
 			while (cur < range['to'].to_i)
 				curNode = DataNode.where(value: cur).first
