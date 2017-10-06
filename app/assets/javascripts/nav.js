7bfea1beca7a83d6292bb25ec4dedb49d2bc9a94
Mitchell Wooden <mwooden@Mitchells-iMac.local>
2017-10-06 14:48:16 -0700

    New homepage layout and js

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -2,14 +2,22 @@ function dropper() {
 	document.getElementById("account_menu").classList.toggle("show");
 }
 
-var v_timeout = setTimeout("timeout_fn()", 3000);
-$('#alert_box').mouseout( function () {
-	v_timeout = setTimeout("timeout_fn()", 3000)
-});
-$('#alert_box').mouseover( function () {
-	clearTimeout(v_timeout);
-});
+
 var timeout_fn = function () {
 	$('#alert_box').fadeOut('fast');
 }
-$("#close").click(timeout_fn);
+var v_timeout = setTimeout("timeout_fn()", 3000);
+
+$(document).on('ready page:load', function() {
+	bind_alert_events();
+});
+
+function bind_alert_events() {
+	$('#alert_box').mouseout( function () {
+		v_timeout = setTimeout("timeout_fn()", 3000)
+	});
+	$('#alert_box').mouseover( function () {
+		clearTimeout(v_timeout);
+	});
+	$("#close").click(timeout_fn);
+}
