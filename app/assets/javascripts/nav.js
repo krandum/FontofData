690c166f6a14612e6abede2eab5c0cf6d55f6f2d
Mitchell Wooden <mwooden@Mitchells-iMac.local>
2017-10-09 11:53:09 -0700

    warning go away

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -5,24 +5,25 @@ function dropper() {
 $(document).on('ready page:load', function() {
 	var close = document.getElementById('close');
 	var alert_box = document.getElementById('alert_box');
-	var timeout_fn = function () {
-		alert_box.fadeOut('fast');
+	if (typeof(alert_box) != 'undefined' && alert_box != null) {
+		var v_timeout = setTimeout(function () {
+			alert_box.fadeOut('fast');
+		}, 3000);
 	}
-	var v_timeout = setTimeout("timeout_fn()", 3000);
-/*	alert_box.addEventListener('mouseover', function(event) {
-		console.log('over');
-		clearTimeout(v_timeout);
-	});
-	alert_box.addEventListener('mouseout', function(event) {
-		v_timeout = setTimeout("timeout_fn()", 3000);
-	});
-	close.addEventListener('click', function(event) {
-		timeout_fn;
-	});
-	document.addEventListener('click', function() {
-		if (document.getElementById("account_menu").classList.contains("show")) {
-			document.getElementById("account_menu").classList.remove("show");
-		}
-	});
-	*/
+	/*	alert_box.addEventListener('mouseover', function(event) {
+	console.log('over');
+	clearTimeout(v_timeout);
+});
+alert_box.addEventListener('mouseout', function(event) {
+v_timeout = setTimeout("timeout_fn()", 3000);
+});
+close.addEventListener('click', function(event) {
+timeout_fn;
+});
+document.addEventListener('click', function() {
+if (document.getElementById("account_menu").classList.contains("show")) {
+document.getElementById("account_menu").classList.remove("show");
+}
+});
+*/
 });
